a2962c5a62ccdafbe928b9ee8036de8c05729570
jtni225 <46794615+jtni225@users.noreply.github.com>
2019-04-27 14:19:06 -0400

    Update Text.py

--- Text.py
+++ Text.py
@@ -3,15 +3,15 @@ from twilio.rest import Client
 
 
 # Your Account Sid and Auth Token from twilio.com/console
-account_sid = 'ACb9dab907e5aca735ea665f370251adb8'
-auth_token = 'a1b4a34038512fe2569f0504710bf947'
+account_sid = ''
+auth_token = ''
 client = Client(account_sid, auth_token)
 
 message = client.messages \
                 .create(
                      body="Treat dispenser on!",
-                     from_='+15023736897',
-                     to='+15024925427'
+                     from_='',
+                     to=''
                  )
 
 print(message.sid)
